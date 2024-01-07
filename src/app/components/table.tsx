7925a77a4b900e2d3d1ae2d392b0b716721b2741
Gil Hanan <gil.hanan@gmail.com>
2024-01-07 20:27:33 +0200

    Table - marked P50 numeric cells

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -1,11 +1,61 @@
-import { Row } from "../runs/models";
+import { Cell, Row } from "../runs/models";
 
 interface Props {
   title: string;
   rows: Row[];
 }
 
+type RowIndexedValue = { value: Cell["value"]; rowIndex: number };
+type CellIndexToSortedValues = {
+  [cellIndex: string]: RowIndexedValue[];
+};
+
+function getCellIndexToSortedValues(rows: Row[]): CellIndexToSortedValues {
+  const cellIndexToSortedValues: CellIndexToSortedValues = {};
+
+  rows.forEach(({ cells }, rowIndex) => {
+    cells.forEach(({ value }, cellIndex) => {
+      if (typeof value !== "number") return;
+
+      if (!cellIndexToSortedValues[cellIndex]) {
+        cellIndexToSortedValues[cellIndex] = [];
+      }
+
+      cellIndexToSortedValues[cellIndex].push({ value, rowIndex });
+    });
+  });
+
+  Object.keys(cellIndexToSortedValues).forEach((key) => {
+    cellIndexToSortedValues[key].sort(({ value: a }, { value: b }) => {
+      if (typeof a === "string" && typeof b === "string") {
+        return a.localeCompare(b);
+      }
+      if (typeof a === "number" && typeof b === "number") {
+        return a - b;
+      }
+      return -1;
+    });
+  });
+
+  return cellIndexToSortedValues;
+}
+
+function getCellIndexToMediansRowIndex(rows: Row[]): {
+  [index: string]: number;
+} {
+  const cellIndexToSortedValues = getCellIndexToSortedValues(rows);
+
+  return Object.keys(cellIndexToSortedValues).reduce((acc, cellIndex) => {
+    const values = cellIndexToSortedValues[cellIndex];
+    const medianIndex = Math.floor(values.length / 2);
+    const medianRowIndex = values[medianIndex].rowIndex;
+    return { ...acc, [cellIndex]: medianRowIndex };
+  }, {});
+}
+
 export default function Table({ title, rows }: Props) {
+  const cellIndexToMediansRowIndex = getCellIndexToMediansRowIndex(rows);
+
   return (
     <div className="inline-flex flex-col gap-4">
       <h2 className="text-lg font-semibold">{title}</h2>
@@ -20,13 +70,21 @@ export default function Table({ title, rows }: Props) {
           </tr>
         </thead>
         <tbody>
-          {rows.map(({ id, cells }) => (
+          {rows.map(({ id, cells }, rowIndex) => (
             <tr key={id}>
-              {cells.map(({ label, value }) => (
-                <td key={`${id}-${label}`} className="border px-4 py-2">
-                  {value?.toString()}
-                </td>
-              ))}
+              {cells.map(({ label, value }, cellIndex) => {
+                const className = `border px-4 py-2 ${
+                  rowIndex === cellIndexToMediansRowIndex[cellIndex]
+                    ? "bg-green-200"
+                    : ""
+                }`;
+
+                return (
+                  <td key={`${id}-${label}`} className={className}>
+                    {value?.toString()}
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
